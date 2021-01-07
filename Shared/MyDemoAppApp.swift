5a993a4bcd8c9574fa446dc7c606d2a8cdc38055
sxc <qiuyequ@icloud.com>
2021-01-07 13:58:28 +0800

    Initial Commit

---
+++ Shared/MyDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoAppApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2021/1/7.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
